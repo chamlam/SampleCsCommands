7303ef1176c25a07afd2116d47f20f87fd5ebeff
dalefugier <dale@mcneel.com>
2014-07-29 13:54:51 -0700

    Added SampleCsAutomateGrasshopper.cs

--- SampleCsCommandsCommand.cs
+++ SampleCsCommandsCommand.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Rhino;
 using Rhino.Commands;
+using Rhino.DocObjects;
 using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
