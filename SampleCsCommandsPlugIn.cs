bfe3d3e700bd123ac6c4a475c9a31549b13e8f3a
Dale Fugier <dale@mcneel.com>
2016-04-20 13:10:57 -0700

    Added StagePlugInToolbarFile function to the plug-in class.

--- SampleCsCommandsPlugIn.cs
+++ SampleCsCommandsPlugIn.cs
@@ -1,4 +1,7 @@
-﻿using Rhino;
+﻿using System;
+using System.IO;
+using System.Text;
+using Rhino;
 using Rhino.PlugIns;
 
 namespace SampleCsCommands
@@ -39,13 +42,76 @@ namespace SampleCsCommands
 
     protected override LoadReturnCode OnLoad(ref string errorMessage)
     {
-      RhinoDoc.CloseDocument += new System.EventHandler<Rhino.DocumentEventArgs>(OnCloseDocument);
+      StagePlugInToolbarFile();
+
+      RhinoDoc.CloseDocument += OnCloseDocument;
       return LoadReturnCode.Success;
     }
 
-    void OnCloseDocument(object sender, Rhino.DocumentEventArgs e)
+    private void OnCloseDocument(object sender, DocumentEventArgs e)
     {
       IsolateIndex = 0;
+    }
+
+    /// <summary>
+    /// Forces the (re)staging of the plug-in's toolbar, or .rui, file.
+    /// </summary>
+    private void StagePlugInToolbarFile()
+    {
+      // QUESTION: 
+      // When we install a newer version of our plugin, with changes to the toolbar,
+      // the new toolbar is not copied over the old toolbar. Is there a way to do this?
+      //
+      // ANSWER:
+      // the first time a plug-in is loaded, Rhino looks for a .rui file with the same
+      // name as the plug-in. If it is found, it is copied to the 
+      // "%APPDATA%\McNeel\Rhinoceros\5.0\Plug-ins\[plug-in name] ([plug-in UUID)\settings" 
+      // folder and opened. It is copied, or staged, to ensure that it is writable and to 
+      // provide a way to get the default one back.
+      //
+      // If your update your plug-in, Rhino will not re-stage the .rui file because it already
+      // exists. You can get Rhino to re-stage the .rui file by deleting it in %APPDATA% and 
+      // restarting which will cause Rhino to copy the file again since it no longer exists. 
+      //
+      // EXAMPLE:
+
+      // Get the version number of our plug-in, that was last used, from our settings file.
+      var plugin_version = Settings.GetString("PlugInVersion", null);
+
+      if (!string.IsNullOrEmpty(plugin_version))
+      {
+        // If the version number of the plug-in that was last used does not match the
+        // version number of this plug-in, proceed.
+        if (0 != string.Compare(Version, plugin_version, StringComparison.OrdinalIgnoreCase))
+        {
+          // Build a path to the user's staged .rui file.
+          var sb = new StringBuilder();
+          sb.Append(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+          sb.Append("\\McNeel\\Rhinoceros\\5.0\\Plug-ins\\");
+          sb.AppendFormat("{0} ({1})", Name, Id);
+          sb.Append("\\settings\\");
+          sb.AppendFormat("{0}.rui", Assembly.GetName().Name);
+
+          var path = sb.ToString();
+
+          // Verify the .rui file exists.
+          if (File.Exists(path))
+          {
+            try
+            {
+              // Delete the .rui file.
+              File.Delete(path);
+            }
+            catch
+            {
+              // ignored
+            }
+          }
+
+          // Save the version number of this plug-in to our settings file.
+          Settings.SetString("PlugInVersion", Version);
+        }
+      }
     }
   }
 }
